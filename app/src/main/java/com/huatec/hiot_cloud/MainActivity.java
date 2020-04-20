518921225acedc0041767eab6aee1c8c56221ca0
zmx <1343665583@qq.com>
2020-04-20 20:18:46 +0800

    添加MVP

--- app/src/main/java/com/huatec/hiot_cloud/MainActivity.java
+++ app/src/main/java/com/huatec/hiot_cloud/MainActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.huatec.hiot_cloud.test.mvptest.model.User;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("tag","test");
+        Log.d("tag", "test");
 
 
     }
 }
+
+
