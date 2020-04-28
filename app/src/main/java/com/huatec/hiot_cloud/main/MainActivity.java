3b89922e11114ec16609a7c3782560fbd3fb32d1
zmx <1343665583@qq.com>
2020-04-28 20:11:07 +0800

    添加主页ui设计

--- app/src/main/java/com/huatec/hiot_cloud/main/MainActivity.java
+++ app/src/main/java/com/huatec/hiot_cloud/main/MainActivity.java
@@ -1,18 +1,24 @@
 package com.huatec.hiot_cloud.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.huatec.hiot_cloud.R;
+import com.huatec.hiot_cloud.base.BaseActivity;
+import com.huatec.hiot_cloud.base.BasePresenter;
+import com.huatec.hiot_cloud.base.BaseView;
 import com.huatec.hiot_cloud.test.mvptest.model.User;
+import com.huatec.hiot_cloud.utils.Constans;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +26,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("tag", "test");
 
+        //设置Viewpaper
+        final ViewPager vpMain = findViewById(R.id.vp_main);
+        vpMain.setAdapter(new MainViewPagerAdapter() );
+        vpMain.setOffscreenPageLimit(Constans.MAIN_FRAGMENT_COUNT);
 
+        RadioGroup rgMain = findViewById(R.id.rg_main);
+        rgMain.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rb_message:
+                        vpMain.setCurrentItem(Constans.MAIN_VIEWPAGER_INDEX_MESSAGE);
+                        break;
+                    case R.id.rb_equipment:
+                        vpMain.setCurrentItem(Constans.MAIN_VIEWPAGER_INDEX_EQUIPMENT);
+                        break;
+                    case R.id.rb_scene:
+                        vpMain.setCurrentItem(Constans.MAIN_VIEWPAGER_INDEX_SCENE);
+                        break;
+                    case R.id.rb_mine:
+                        vpMain.setCurrentItem(Constans.MAIN_VIEWPAGER_INDEX_MINE);
+                        break;
+                        default:
+                }
+            }
+        });
+    }
+
+    @Override
+    public BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    public void injectIndependies() {
+    getActivityComponent().inject(this);
     }
 }
 
--- app/src/main/java/com/huatec/hiot_cloud/test/mvptest/TestMVPActivity.java
+++ app/src/main/java/com/huatec/hiot_cloud/test/mvptest/TestMVPActivity.java
@@ -24,7 +24,7 @@ public class TestMVPActivity extends BaseActivity<TestView, TestPresenter> imple
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        getActivityComponent().inject(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_mvp);
         final EditText etUserName = findViewById(R.id.et_user_name);
@@ -46,6 +46,11 @@ public class TestMVPActivity extends BaseActivity<TestView, TestPresenter> imple
     @Override
     public TestPresenter createPresenter() {
         return presenter;
+    }
+
+    @Override
+    public void injectIndependies() {
+        getActivityComponent().inject(this);
     }
 
     @Override
